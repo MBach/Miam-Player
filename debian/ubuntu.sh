83570ce803cf68c5c02ce8182d16e6be36048c07
Matthieu BACHELIER <bachelier.matthieu@gmail.com>
2015-10-25 22:26:27 +0100

    Fix libtag dependency

--- debian/ubuntu.sh
+++ debian/ubuntu.sh
@@ -19,7 +19,7 @@ for D in ${DISTRIBUTIONS[@]}; do
   VER=0.8.0~`git log -1 --pretty=format:"git${DATE}.%h~${D}" 2> /dev/null`
   mkchangelog $VER $D
   debuild -S -sa -k7CA4455D
-  #dput -f ppa:bachelier-matthieu/ppa ../miam-player_${VER}_source.changes
+  dput -f ppa:bachelier-matthieu/ppa ../miam-player_${VER}_source.changes
   cp -avf $CHANGELOG_BAK debian/changelog
 done
 
